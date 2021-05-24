cb27db73b5b490f2fbf8902ae14d0a60e67fede3
Junhua <60574233+JunhuaChu@users.noreply.github.com>
2021-05-24 09:55:32 +0200

    Update sem-af.utils.ts
    add some buttons based on ISO standard

--- src/app/components/tools/sem-af/sem-af.utils.ts
+++ src/app/components/tools/sem-af/sem-af.utils.ts
@@ -12,6 +12,61 @@ export const defaultAnnotationClasses: IAnnotationClass[] = [
     visible: true
   },
   {
+    rgb: 'rgb(131,120,120)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.QSLink',
+    name: 'QSLink',
+    sort: 1,
+    concept: true,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-QSLink',
+    visible: true
+  },
+  {
+    rgb: 'rgb(131,120,120)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.MetaLink',
+    name: 'Meta Link',
+    sort: 1,
+    concept: true,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-MetaLink',
+    visible: true
+  },
+  {
+    rgb: 'rgb(131,120,120)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.MeasureLink',
+    name: 'Measure Link',
+    sort: 1,
+    concept: true,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-MeasureLink',
+    visible: true
+  },
+  {
+    rgb: 'rgb(131,120,120)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.MoveLink',
+    name: 'Move Link',
+    sort: 1,
+    concept: true,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-MoveLink',
+    visible: true
+  },
+  {
+    rgb: 'rgb(131,120,120)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.OLink',
+    name: 'OLink',
+    sort: 1,
+    concept: true,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-OLink',
+    visible: true
+  },
+  {
     rgb: 'rgb(177, 89, 40)',
     type: 'org.texttechnologylab.annotation.semaf.isospace.Place',
     name: 'Place',
@@ -23,6 +78,28 @@ export const defaultAnnotationClasses: IAnnotationClass[] = [
     visible: true
   },
 
+  {
+    rgb: 'rgb(120, 64, 40)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.Location',
+    name: 'Location',
+    sort: 1,
+    concept: false,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-Location',
+    visible: true
+  },
+  {
+    rgb: 'rgb(120, 0, 40)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.Nonmotion_Event',
+    name: 'Nonmotion_Event',
+    sort: 1,
+    concept: false,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-Nonmotion_Event',
+    visible: true
+  },
 
   {
     rgb: 'rgb(233,234,192)',
@@ -47,7 +124,7 @@ export const defaultAnnotationClasses: IAnnotationClass[] = [
     visible: true
   },
   {
-    rgb: 'rgb(0,255,255)',
+    rgb: 'rgb(137,157,192)',
     type: 'org.texttechnologylab.annotation.semaf.isospace.Motion',
     name: 'Motion',
     sort: 1,
@@ -57,19 +134,21 @@ export const defaultAnnotationClasses: IAnnotationClass[] = [
     value: 'I-Motion',
     visible: true
   },
+  
+ 
   {
-    rgb: 'rgb(128,0,128)',
-    type: 'org.texttechnologylab.annotation.semaf.isospace.SRelation',
-    name: 'sRelation',
+    rgb: 'rgb(131,50,50)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.URL',
+    name: 'URL',
     sort: 1,
     concept: false,
     stype: 'ner',
     css: 'OTHER',
-    value: 'I-SRelation',
+    value: 'URL',
     visible: true
   },
   {
-    rgb: 'rgb(255,0,255)',
+    rgb: 'rgb(160,191,124)',
     type: 'org.texttechnologylab.annotation.semaf.isospace.Measure',
     name: 'Measure',
     sort: 1,
@@ -80,7 +159,7 @@ export const defaultAnnotationClasses: IAnnotationClass[] = [
     visible: true
   },
   {
-    rgb: 'rgb(0,0,128)',
+    rgb: 'rgb(252,157,154)',
     type: 'org.texttechnologylab.annotation.semaf.isospace.MRelation',
     name: 'MRelation',
     sort: 1,
@@ -89,10 +168,52 @@ export const defaultAnnotationClasses: IAnnotationClass[] = [
     css: 'OTHER',
     value: 'I-UNK',
     visible: true
-  }
-
+  },
+  {
+    rgb: 'rgb(131,120,120)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.CP',
+    name: 'CP',
+    sort: 1,
+    concept: false,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-CP',
+    visible: true
+  },
+  {
+    rgb: 'rgb(131,120,120)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.Spatial_Signal',
+    name: 'Spatial Signal',
+    sort: 1,
+    concept: false,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-Spatial_Signal',
+    visible: true
+  },
+  {
+    rgb: 'rgb(131,120,120)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.Motion_Signal',
+    name: 'Motion Signal',
+    sort: 1,
+    concept: false,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-Motion_Signal',
+    visible: true
+  },
+  {
+    rgb: 'rgb(131,120,120)',
+    type: 'org.texttechnologylab.annotation.semaf.isospace.Spatial_Function',
+    name: 'Spatial Function',
+    sort: 1,
+    concept: false,
+    stype: 'ner',
+    css: 'OTHER',
+    value: 'I-Spatial_Function',
+    visible: true
+  },
 ];
-
 
 export interface IAnnotationClass {
   name: string;
